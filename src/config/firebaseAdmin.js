7ddf3c8fcf1096049b4e0b06f292632892a0ad95
Arun BAarode <developer@mindcrawller.com>
2026-08-18 17:11:44 +0530

    fix firebase admin railway config

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -1,11 +1,28 @@
-import { cert, getApps, initializeApp } from "firebase-admin/app";
-import { getMessaging } from "firebase-admin/messaging";
-import serviceAccount from "./serviceAccountKey.json" with { type: "json" };
+// import { cert, getApps, initializeApp } from "firebase-admin/app";
+// import { getMessaging } from "firebase-admin/messaging";
+// import serviceAccount from "./serviceAccountKey.json" with { type: "json" };
 
-if (getApps().length === 0) {
-  initializeApp({
-    credential: cert(serviceAccount),
+// if (getApps().length === 0) {
+//   initializeApp({
+//     credential: cert(serviceAccount),
+//   });
+// }
+
+// export default getMessaging();
+import admin from "firebase-admin";
+
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT is missing");
+}
+
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
-export default getMessaging();
+export default admin;
