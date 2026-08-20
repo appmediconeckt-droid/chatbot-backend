80cf4cefe841b82dbbc0e0e00855c6bc92d0aade
Arun BAarode <developer@mindcrawller.com>
2026-08-20 16:26:44 +0530

    change

--- src/app.js
+++ src/app.js
@@ -402,7 +402,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
